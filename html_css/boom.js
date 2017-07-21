f3e11974c3a3f6757773e3b10bfbf1127ed531e1
toChaim <toChaim@gmail.com>
2017-07-20 19:11:58 -0700

    start recucive function.

--- html_css/boom.js
+++ html_css/boom.js
@@ -3,19 +3,25 @@ $(document).ready(function () {
 		$p = $(this).children('p').eq(0);
 		$p.css('visibility', 'visible')
 
-		var $boom = $('<div>', {
-			class: 'explosion',
-			width: '0px',
-			height: '0px',
-		});
-		$(this).append($boom);
 		var radius = Math.floor(Math.random()*75) + 50;
-		$boom.animate({ 
-			width: radius,
-			height: radius,
-			left: '-=' + (radius)/2,
-			top: '-=' + (radius)/2, 
-		});
+		makeBoom($(this), radius);
+
+		function makeBoom($obj, radius) {
+			var $boom = $('<div>', {
+				class: 'explosion',
+				width: '0px',
+				height: '0px',
+			});
+
+			$obj.append($boom)
+
+			$boom.animate({ 
+				width: radius,
+				height: radius,
+				left: '-=' + (radius)/2,
+				top: '-=' + (radius)/2, 
+			}, 2000);
+		}
 	});
 
 	$('#reset').on('click', function () {
